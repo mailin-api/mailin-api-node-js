2fefa96347d202109ca8aa94374565ac8431a13d
Kapil Sharma <kapil.sharma@silverlineit.com>
2014-01-24 14:31:07 +0530

    Tutorial updated

--- examples/tutorial.js
+++ examples/tutorial.js
@@ -1,7 +1,7 @@
 // Include mailin
 require("../mailin.js");
 // Initialize mailin object
-var client = new Mailin("https://api.mailinblue.com/v1.0","<access key>","<secret key>");
+var client = new Mailin("https://api.sendinblue.com/v1.0","<access key>","<secret key>");
 // Get list of all campaigns in account
 client.get_campaigns().on('complete', function(data) {
 	console.log(data);
